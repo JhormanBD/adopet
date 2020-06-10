b5a71e405d6c1c9f00634e9a893b2ffd5c51cc20
Daniel Caballero <danielcaballero796@gmail.com>
2020-06-09 19:10:32 -0500

    correccion del CRUD convenios

--- controller/ConvenioController_Edit.php
+++ controller/ConvenioController_Edit.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,25 +8,24 @@
 
 include_once realpath('../facade/ConvenioFacade.php');
 $idConvenio = strip_tags($_POST['idConvenio']);
-$Veterinaria_idVeterinaria = strip_tags($_POST['Veterinaria_idVeterinaria']);
-       
-        $Fundacion_idFundacion = strip_tags($_POST['Fundacion_idFundacion']);
-        
-        $fechaConvenio = strip_tags($_POST['fechaConvenio']);
-        $nombreConvenio = strip_tags($_POST['nombreConvenio']);
-        $duracionConvenio = strip_tags($_POST['duracionConvenio']);
-        $respuesta=false;
-       //guiarse por los datos que recibe el insert
-        if ($idConvenio===''||$Veterinaria_idVeterinaria===''||$Fundacion_idFundacion===''||$fechaConvenio===''||$nombreConvenio===''||$duracionConvenio===''){
-            
-            return $respuesta;
-        }else{
-            
-         //insert devuelve es un numero si incerto   
-         $respuesta=ConvenioFacade::update($idConvenio, $veterinaria_idVeterinaria, $fundacion_idFundacion, $fechaConvenio, $nombreConvenio, $duracionConvenio);
-        if($respuesta>0){
-            return $respuesta=true;
-        }
-            
-        }
-return true;
+$Vinculacion_idVeterinaria = strip_tags($_POST['Veterinaria_idVeterinaria']);
+$Fundacion_idFundacion = strip_tags($_POST['Fundacion_idFundacion']);
+$fechaConvenio = strip_tags($_POST['fechaConvenio']);
+$nombreConvenio = strip_tags($_POST['nombreConvenio']);
+$duracionConvenio = strip_tags($_POST['duracionConvenio']);
+$respuesta = false;
+//guiarse por los datos que recibe el insert
+if ($idConvenio === '' || $Vinculacion_idVeterinaria === '' || $Fundacion_idFundacion === '' || $fechaConvenio === '' || $nombreConvenio === '' || $duracionConvenio === '') {
+
+    return $respuesta;
+} else {
+
+    //insert devuelve es un numero si incerto   
+    $respuesta = ConvenioFacade::update($idConvenio, $Vinculacion_idVeterinaria, $fundacion_idFundacion, $fechaConvenio, $nombreConvenio, $duracionConvenio);
+    if ($respuesta > 0) {
+        return $respuesta = true;
+    } else {
+        return $respuesta;
+    }
+}
+    
--- facade/ConvenioFacade.php
+++ facade/ConvenioFacade.php
@@ -1,8 +1,9 @@
 <?php
+
 /*
-              -------Creado por-------
-             \(x.x )/ Anarchy \( x.x)/
-              ------------------------
+  -------Creado por-------
+  \(x.x )/ Anarchy \( x.x)/
+  ------------------------
  */
 
 //    La segunda regla de Anarchy es no hablar de Anarchy  \\
@@ -16,119 +17,132 @@ require_once realpath('../dto/Fundacion.php');
 
 class ConvenioFacade {
 
-  /**
-   * Para su comodidad, defina aquí el gestor de conexión predilecto para esta entidad
-   * @return idGestor Devuelve el identificador del gestor de conexión
-   */
-  private static function getGestorDefault(){
-      return DEFAULT_GESTOR;
-  }
-  /**
-   * Para su comodidad, defina aquí el nombre de base de datos predilecto para esta entidad
-   * @return dbName Devuelve el nombre de la base de datos a emplear
-   */
-  private static function getDataBaseDefault(){
-      return DEFAULT_DBNAME;
-  }
-  /**
-   * Crea un objeto Convenio a partir de sus parámetros y lo guarda en base de datos.
-   * Puede recibir NullPointerException desde los métodos del Dao
-   * @param idConvenio
-   * @param veterinaria_idVeterinaria
-   * @param fundacion_idFundacion
-   * @param fechaConvenio
-   * @param nombreConvenio
-   * @param duracionConvenio
-   * @param estado
-   */
-  public static function insert( $idConvenio,  $veterinaria_idVeterinaria,  $fundacion_idFundacion,  $fechaConvenio,  $nombreConvenio,  $duracionConvenio,  $estado){
-      $convenio = new Convenio();
-      $convenio->setIdConvenio($idConvenio); 
-      $convenio->setVeterinaria_idVeterinaria($veterinaria_idVeterinaria); 
-      $convenio->setFundacion_idFundacion($fundacion_idFundacion); 
-      $convenio->setFechaConvenio($fechaConvenio); 
-      $convenio->setNombreConvenio($nombreConvenio); 
-      $convenio->setDuracionConvenio($duracionConvenio); 
-      $convenio->setEstado($estado); 
+    /**
+     * Para su comodidad, defina aquí el gestor de conexión predilecto para esta entidad
+     * @return idGestor Devuelve el identificador del gestor de conexión
+     */
+    private static function getGestorDefault() {
+        return DEFAULT_GESTOR;
+    }
 
-     $FactoryDao=new FactoryDao(self::getGestorDefault());
-     $convenioDao =$FactoryDao->getconvenioDao(self::getDataBaseDefault());
-     $rtn = $convenioDao->insert($convenio);
-     $convenioDao->close();
-     return $rtn;
-  }
+    /**
+     * Para su comodidad, defina aquí el nombre de base de datos predilecto para esta entidad
+     * @return dbName Devuelve el nombre de la base de datos a emplear
+     */
+    private static function getDataBaseDefault() {
+        return DEFAULT_DBNAME;
+    }
 
-  /**
-   * Selecciona un objeto Convenio de la base de datos a partir de su(s) llave(s) primaria(s).
-   * Puede recibir NullPointerException desde los métodos del Dao
-   * @param idConvenio
-   * @return El objeto en base de datos o Null
-   */
-  public static function select($idConvenio){
-      $convenio = new Convenio();
-      $convenio->setIdConvenio($idConvenio); 
+    /**
+     * Crea un objeto Convenio a partir de sus parámetros y lo guarda en base de datos.
+     * Puede recibir NullPointerException desde los métodos del Dao
+     * @param idConvenio
+     * @param veterinaria_idVeterinaria
+     * @param fundacion_idFundacion
+     * @param fechaConvenio
+     * @param nombreConvenio
+     * @param duracionConvenio
+     * @param estado
+     */
+    public static function insert($veterinaria_idVeterinaria, $fundacion_idFundacion, $fechaConvenio, $nombreConvenio, $duracionConvenio) {
+        $convenio = new Convenio();
+        $convenio->setVeterinaria_idVeterinaria($veterinaria_idVeterinaria);
+        $convenio->setFundacion_idFundacion($fundacion_idFundacion);
+        $convenio->setFechaConvenio($fechaConvenio);
+        $convenio->setNombreConvenio($nombreConvenio);
+        $convenio->setDuracionConvenio($duracionConvenio);
 
-     $FactoryDao=new FactoryDao(self::getGestorDefault());
-     $convenioDao =$FactoryDao->getconvenioDao(self::getDataBaseDefault());
-     $result = $convenioDao->select($convenio);
-     $convenioDao->close();
-     return $result;
-  }
+        $FactoryDao = new FactoryDao(self::getGestorDefault());
+        $convenioDao = $FactoryDao->getconvenioDao(self::getDataBaseDefault());
+        $rtn = $convenioDao->insert($convenio);
+        $convenioDao->close();
+        return $rtn;
+    }
 
-  /**
-   * Modifica los atributos de un objeto Convenio  ya existente en base de datos.
-   * Puede recibir NullPointerException desde los métodos del Dao
-   * @param idConvenio
-   * @param veterinaria_idVeterinaria
-   * @param fundacion_idFundacion
-   * @param fechaConvenio
-   * @param nombreConvenio
-   * @param duracionConvenio
-   * @param estado
-   */
-  public static function update($idConvenio, $veterinaria_idVeterinaria, $fundacion_idFundacion, $fechaConvenio, $nombreConvenio, $duracionConvenio, $estado){
-      $convenio = self::select($idConvenio);
-      $convenio->setVeterinaria_idVeterinaria($veterinaria_idVeterinaria); 
-      $convenio->setFundacion_idFundacion($fundacion_idFundacion); 
-      $convenio->setFechaConvenio($fechaConvenio); 
-      $convenio->setNombreConvenio($nombreConvenio); 
-      $convenio->setDuracionConvenio($duracionConvenio); 
-      $convenio->setEstado($estado); 
+    /**
+     * Selecciona un objeto Convenio de la base de datos a partir de su(s) llave(s) primaria(s).
+     * Puede recibir NullPointerException desde los métodos del Dao
+     * @param idConvenio
+     * @return El objeto en base de datos o Null
+     */
+    public static function select($idConvenio) {
+        $convenio = new Convenio();
+        $convenio->setIdConvenio($idConvenio);
 
-     $FactoryDao=new FactoryDao(self::getGestorDefault());
-     $convenioDao =$FactoryDao->getconvenioDao(self::getDataBaseDefault());
-     $convenioDao->update($convenio);
-     $convenioDao->close();
-  }
+        $FactoryDao = new FactoryDao(self::getGestorDefault());
+        $convenioDao = $FactoryDao->getconvenioDao(self::getDataBaseDefault());
+        $result = $convenioDao->select($convenio);
+        $convenioDao->close();
+        return $result;
+    }
 
-  /**
-   * Elimina un objeto Convenio de la base de datos a partir de su(s) llave(s) primaria(s).
-   * Puede recibir NullPointerException desde los métodos del Dao
-   * @param idConvenio
-   */
-  public static function delete($idConvenio){
-      $convenio = new Convenio();
-      $convenio->setIdConvenio($idConvenio); 
+    /**
+     * Modifica los atributos de un objeto Convenio  ya existente en base de datos.
+     * Puede recibir NullPointerException desde los métodos del Dao
+     * @param idConvenio
+     * @param veterinaria_idVeterinaria
+     * @param fundacion_idFundacion
+     * @param fechaConvenio
+     * @param nombreConvenio
+     * @param duracionConvenio
+     * @param estado
+     */
+    public static function update($idConvenio, $veterinaria_idVeterinaria, $fundacion_idFundacion, $fechaConvenio, $nombreConvenio, $duracionConvenio) {
+        $convenio = self::select($idConvenio);
+        $convenio->setVeterinaria_idVeterinaria($veterinaria_idVeterinaria);
+        $convenio->setFundacion_idFundacion($fundacion_idFundacion);
+        $convenio->setFechaConvenio($fechaConvenio);
+        $convenio->setNombreConvenio($nombreConvenio);
+        $convenio->setDuracionConvenio($duracionConvenio);
 
-     $FactoryDao=new FactoryDao(self::getGestorDefault());
-     $convenioDao =$FactoryDao->getconvenioDao(self::getDataBaseDefault());
-     $convenioDao->delete($convenio);
-     $convenioDao->close();
-  }
+        $FactoryDao = new FactoryDao(self::getGestorDefault());
+        $convenioDao = $FactoryDao->getconvenioDao(self::getDataBaseDefault());
+        $convenioDao->update($convenio);
+        $convenioDao->close();
+    }
 
-  /**
-   * Lista todos los objetos Convenio de la base de datos.
-   * Puede recibir NullPointerException desde los métodos del Dao
-   * @return $result Array con los objetos Convenio en base de datos o Null
-   */
-  public static function listAll(){
-     $FactoryDao=new FactoryDao(self::getGestorDefault());
-     $convenioDao =$FactoryDao->getconvenioDao(self::getDataBaseDefault());
-     $result = $convenioDao->listAll();
-     $convenioDao->close();
-     return $result;
-  }
+    /**
+     * Cambia el estado un objeto Convenio de la base de datos a partir de su(s) llave(s) primaria(s).
+     * Puede recibir NullPointerException desde los métodos del Dao
+     * @param idConvenio
+     */
+    public static function updateEliminar($idConvenio) {
+        $convenio = self::select($idConvenio);
 
+        $FactoryDao = new FactoryDao(self::getGestorDefault());
+        $convenioDao = $FactoryDao->getconvenioDao(self::getDataBaseDefault());
+        $convenioDao->updateEliminar($convenio);
+        $convenioDao->close();
+    }
+
+    /**
+     * Elimina un objeto Convenio de la base de datos a partir de su(s) llave(s) primaria(s).
+     * Puede recibir NullPointerException desde los métodos del Dao
+     * @param idConvenio
+     */
+    public static function delete($idConvenio) {
+        $convenio = new Convenio();
+        $convenio->setIdConvenio($idConvenio);
+
+        $FactoryDao = new FactoryDao(self::getGestorDefault());
+        $convenioDao = $FactoryDao->getconvenioDao(self::getDataBaseDefault());
+        $convenioDao->delete($convenio);
+        $convenioDao->close();
+    }
+
+    /**
+     * Lista todos los objetos Convenio de la base de datos.
+     * Puede recibir NullPointerException desde los métodos del Dao
+     * @return $result Array con los objetos Convenio en base de datos o Null
+     */
+    public static function listAll() {
+        $FactoryDao = new FactoryDao(self::getGestorDefault());
+        $convenioDao = $FactoryDao->getconvenioDao(self::getDataBaseDefault());
+        $result = $convenioDao->listAll();
+        $convenioDao->close();
+        return $result;
+    }
 
 }
+
 //That`s all folks!
