3587d9d2b3273bbff4eed63a322d3ec7cc6e8322
Andres Ariza <adac021298@gmail.com>
2020-06-11 00:04:19 -0500

    Correciones CRUD DOcumento y FavoritoMascotasUsuario

--- controller/FavoritomascotausuarioController_Insert.php
+++ controller/FavoritomascotausuarioController_Insert.php
@@ -1,16 +1,16 @@
 <?php
 include_once realpath('../facade/FavoritomascotausuarioFacade.php');
 
-$idMascota = strip_tags($_POST['idMascota']);
+//$idMascota = strip_tags($_POST['idMascota']);
 $mascota           = new Mascota();
-$mascota->setIdMascota($idMascota);
-$idUsuario = strip_tags($_POST['idUsuario']);
+$mascota->setIdMascota(2);
+//$idUsuario = strip_tags($_POST['idUsuario']);
 $usuario           = new Usuario();
-$usuario->setIdUsuario($idUsuario);
+$usuario->setIdUsuario(1);
 $respuesta = false;
-if ($idMascota === '' || $idUsuario === '') {
-    echo $respuesta;
-} else {
+//if ($idMascota === '' || $idUsuario === '') {
+//    echo $respuesta;
+//} else {
     $respuesta = FavoritomascotausuarioFacade::insert($mascota, $usuario);
     if ($respuesta > 0) {
         echo $respuesta = true;
